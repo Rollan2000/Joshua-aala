1d717acb05469fd97337a5ca421f7d10c291d266
Rollan2000 <105805669+Rollan2000@users.noreply.github.com>
2024-03-18 14:53:35 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// Can also be included with a regular script tag
+('#menu-icon,close-icon').click(function () {
+    $('nav').toggleClass("navexpanded");
+    return false;
+
+    // Code will launch when the page has loaded
+    
+});
+
+
+
